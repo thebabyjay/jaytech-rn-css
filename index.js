a9c058c817c4fcf7c9b908d0fe39fee4615d3b74
Jason Barnett <jayb21@neo.rr.com>
2020-01-16 23:25:18 -0500

    added color palette

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// STYLES
 const containerStyles = require("./containers");
 const flexStyles = require("./flex");
 const marginAndPaddingStyles = require("./marginAndPadding");
 const textStyles = require("./text");
+
+// CONSTANTS
+const colors = require("./colors");
 
 const mergedStyles = Object.assign(
   {},
@@ -11,6 +15,8 @@ const mergedStyles = Object.assign(
   flexStyles
 );
 
-console.log(mergedStyles)
+// console.log(mergedStyles)
+// console.log(colors);
 
 module.exports = mergedStyles;
+exports.colors = colors;
